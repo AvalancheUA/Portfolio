cf04111d4f4bb48e127e2764271ab94de512c5d8
AvalancheUA <avalanche.valentina@gmail.com>
2023-02-28 22:14:04 +0200

    lesson-21

---
+++ lesson-21/script.js
@@ -0,0 +1,9 @@
+let firsNumber;
+let secondNumber;
+
+firsNumber= Number(prompt('enter number'));
+secondNumber= Number(prompt('enter number'));
+
+result = (firsNumber+secondNumber)+" "+ (firsNumber-secondNumber) + " " + (firsNumber*secondNumber) + " " + (firsNumber/secondNumber);
+
+alert (result)
